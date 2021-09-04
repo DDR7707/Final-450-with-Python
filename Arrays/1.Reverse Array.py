1ed56792cb392a8a0867226ba49fffa0ae8035cf
DINESHREDDY <72461021+DDR7707@users.noreply.github.com>
2021-09-04 19:41:45 +0530

    Update 1.Reverse Array.py

--- Arrays/1.Reverse Array.py
+++ Arrays/1.Reverse Array.py
@@ -1,2 +1,15 @@
 def reverse(arr):
   return arr[::-1]
+
+#or
+
+def reverse(arr):
+  l = 0
+  r = len(arr) - 1
+  
+  while l < r:
+    arr[l] , arr[r] = arr[r] , arr[l]
+    l += 1
+    r -= 1
+  return arr
+
