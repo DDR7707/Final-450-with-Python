857d50bb00dbca7c1f7a47c0ca40b6ba199f505c
DINESHREDDY <72461021+DDR7707@users.noreply.github.com>
2021-09-05 20:57:55 +0530

    easy

---
+++ Stacks and Queues/303.Get Min in Stack with O(1).py
@@ -0,0 +1,8 @@
+# Implement auxillary stack with min upto now
+if a = [18,19,25,15,16]
+stack a , stack mini:
+18 | 18
+18 , 19 | 18 , 18
+18 , 19 , 29 | 18 , 18 , 18
+18 , 19 , 29 , 15 | 18 , 18 , 18 , 15
+18 , 19 , 29 , 15 , 16 | 18 , 18 , 18 , 15 , 15
