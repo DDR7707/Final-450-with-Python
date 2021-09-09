baf24cd69eb3d71815819a9a615d32fa08ef8b50
DINESHREDDY <72461021+DDR7707@users.noreply.github.com>
2021-09-09 23:38:57 +0530

    easy+

---
+++ Dynamic Programming/444.Largest Sum Contiguous Subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr,N):
+        ##Your code here
+        max_comp = 0
+        max_far = arr[0]
+        
+        for i in range(N):
+            max_comp += arr[i]
+            if max_comp > max_far:
+                max_far = max_comp
+            if max_comp < 0:
+                max_comp = 0
+        return max_far
