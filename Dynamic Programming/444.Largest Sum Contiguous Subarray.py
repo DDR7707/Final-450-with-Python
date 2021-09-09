d8b24c21f0aabaf67ba81487a67b93537443deb8
DINESHREDDY <72461021+DDR7707@users.noreply.github.com>
2021-09-09 23:42:37 +0530

    easy+ . check dp approach also

--- Dynamic Programming/444.Largest Sum Contiguous Subarray.py
+++ Dynamic Programming/444.Largest Sum Contiguous Subarray.py
@@ -13,3 +13,15 @@ class Solution:
             if max_comp < 0:
                 max_comp = 0
         return max_far
+
+    
+def maxSubArraySum(a,size):
+     
+    max_so_far =a[0]
+    curr_max = a[0]
+     
+    for i in range(1,size):
+        curr_max = max(a[i], curr_max + a[i])
+        max_so_far = max(max_so_far,curr_max)
+         
+    return max_so_far    
