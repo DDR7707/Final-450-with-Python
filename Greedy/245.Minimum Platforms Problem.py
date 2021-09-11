5b3e8984475ad7d95fcf576c176c51661a2024bf
DINESHREDDY <72461021+DDR7707@users.noreply.github.com>
2021-09-11 22:08:46 +0530

    medium , imp* linear approach

--- Greedy/245.Minimum Platforms Problem.py
+++ Greedy/245.Minimum Platforms Problem.py
@@ -19,3 +19,44 @@ class Solution:
             final = max(final , maxi)
         
         return final  
+
+    
+ # Linear Approach
+    
+def minPlatform(arrival, departure, n):
+ 
+    # As time range from 0 to 2359 in
+    # 24 hour clock, we declare an array
+    # for values from 0 to 2360
+    platform = [0] * 2631
+    requiredPlatform = 1
+     
+    for i in range(n):
+ 
+        # Increment the platforms for arrival
+        platform[arrival[i]] += 1
+ 
+        # Once train departs we decrease the
+        # platform count
+        platform[departure[i] + 1] -= 1
+ 
+    # We are running loop till 2361 because
+    # maximum time value in a day can be 23:60
+    for i in range(1, 2631):
+ 
+        # Taking cumulative sum of
+        # platform give us required
+        # number of platform fro every minute
+        platform[i] = platform[i] + platform[i - 1]
+        requiredPlatform = max(requiredPlatform,
+                               platform[i])
+         
+    return requiredPlatform
+ 
+# Driver code
+arr = [ 900, 940, 950, 1100, 1500, 1800 ]
+dep = [ 910, 1200, 1120, 1130, 1900, 2000 ]
+n = len(arr)
+ 
+print("Minimum Number of Platforms Required = ",
+       minPlatform(arr, dep, n))    
