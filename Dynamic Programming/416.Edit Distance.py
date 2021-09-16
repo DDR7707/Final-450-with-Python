21b9afd00468570e5b6c6d1c3dafbd5385a4c39f
DINESHREDDY <72461021+DDR7707@users.noreply.github.com>
2021-09-16 19:11:05 +0530

    medium+ , *imp

---
+++ Dynamic Programming/416.Edit Distance.py
@@ -0,0 +1,19 @@
+class Solution:
+	def editDistance(self, s, t):
+		# Code here
+        dp = [[0 for i in range(len(t)+1)] for i in range(len(s)+1)]
+    
+        for i in range(1,len(s)+1):
+            dp[i][0] = i
+    
+        for i in range(1 , len(t)+1):
+            dp[0][i] = i
+    
+        for i in range(1,len(s)+1):
+            for j in range(1,len(t)+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                     dp[i][j] = 1 + min(dp[i-1][j] , dp[i][j-1] , dp[i-1][j-1])
+                     
+        return dp[len(s)][len(t)]
