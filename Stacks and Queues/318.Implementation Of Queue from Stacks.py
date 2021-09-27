d1c0ec97e477d28daba8ef8be695ed35a1a9a442
DINESHREDDY <72461021+DDR7707@users.noreply.github.com>
2021-09-27 23:34:52 +0530

    easy++ , ****tricky

---
+++ Stacks and Queues/318.Implementation Of Queue from Stacks.py
@@ -0,0 +1,30 @@
+'''
+enQueue(q,  x)
+
+While stack1 is not empty, push everything from stack1 to stack2.
+Push x to stack1 (assuming size of stacks is unlimited).
+Push everything back to stack1.
+Here time complexity will be O(n)
+
+deQueue(q): 
+
+If stack1 is empty then error
+Pop an item from stack1 and return it
+
+
+
+
+
+
+
+enQueue(q,  x)
+  1) Push x to stack1 (assuming size of stacks is unlimited).
+Here time complexity will be O(1)
+
+deQueue(q)
+  1) If both stacks are empty then error.
+  2) If stack2 is empty
+       While stack1 is not empty, push everything from stack1 to stack2.
+  3) Pop the element from stack2 and return it.
+Here time complexity will be O(n)
+'''
